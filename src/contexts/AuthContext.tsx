8bbc30edc3e74ae389374dbf803b654e924e7de8
Roberto <beto.rodriguesds@gmail.com>
2022-11-02 19:25:48 -0300

    feat: oAuth com google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,11 @@
-import React, { createContext, ReactNode } from 'react';
+import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
+
+import { clientId } from '../configs';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -8,6 +15,8 @@ interface UserProps {
 export interface AuthContextDataProps {
   user: UserProps;
   signIn: () => Promise<void>;
+  isUserLoading: boolean;
+  isAuthenticated?: boolean;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
@@ -17,11 +26,39 @@ interface AuthProviderProps {
 }
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-  const user: UserProps = {
-    name: 'Roberto',
-    avatarUrl: 'https://github.com/robertorsjr.png',
-  };
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
 
-  async function signIn() {}
-  return <AuthContext.Provider value={{ signIn, user }}>{children}</AuthContext.Provider>;
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId,
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  async function signInWithGoogle(accessToken: string) {
+    console.log(accessToken);
+  }
+
+  async function signIn() {
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+  return (
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated: !!user, isUserLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
