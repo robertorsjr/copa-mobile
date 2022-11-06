cc07276d7663174d0ed9ff87e23ab89f7abe14ee
Roberto <beto.rodriguesds@gmail.com>
2022-11-06 13:20:04 -0300

    feat: bottom tab

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,9 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'native-base';
+import { PlusCircle, SoccerBall } from 'phosphor-react-native';
 import React from 'react';
+// eslint-disable-next-line import/namespace
+import { Platform } from 'react-native';
 
 import { New } from '../screens/New';
 import { Pools } from '../screens/Pools';
@@ -7,10 +11,44 @@ import { Pools } from '../screens/Pools';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const { colors, sizes } = useTheme();
+
+  const iconSize = sizes[6];
+
   return (
-    <Navigator>
-      <Screen name="new" component={New} />
-      <Screen name="pools" component={Pools} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarActiveTintColor: colors.yellow[500],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarStyle: {
+          position: 'absolute',
+          height: sizes[22],
+          borderTopWidth: 0,
+          backgroundColor: colors.gray[800],
+        },
+        tabBarItemStyle: {
+          position: 'relative',
+          top: Platform.OS === 'android' ? -10 : 0,
+        },
+      }}>
+      <Screen
+        name="new"
+        component={New}
+        options={{
+          tabBarIcon: ({ color }) => <PlusCircle color={color} size={iconSize} />,
+          tabBarLabel: 'Novo bolão',
+        }}
+      />
+      <Screen
+        name="pools"
+        component={Pools}
+        options={{
+          tabBarIcon: ({ color }) => <SoccerBall color={color} size={iconSize} />,
+          tabBarLabel: 'Meus bolões',
+        }}
+      />
     </Navigator>
   );
 }
